a593c266bdaa02de6b0168d4bb415a9f50cbe98d
detarmstrong <detarmstrong@gmail.com>
2011-04-11 23:18:10 -0700

    cleansup unused comments and imports

--- src/com/futilities/mindtimer/IntervalPicker.java
+++ src/com/futilities/mindtimer/IntervalPicker.java
@@ -23,7 +23,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class IntervalPicker extends Activity implements OnClickListener {
     private final static String TAG = "numpicker";
--- src/com/futilities/mindtimer/IntervalPickerNumPadView.java
+++ src/com/futilities/mindtimer/IntervalPickerNumPadView.java
@@ -21,8 +21,6 @@ import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AnimationUtils;
-import android.widget.Toast;
 
 public class IntervalPickerNumPadView extends View {
     private static final String TAG = "INTERVALPICKERVIEW";
